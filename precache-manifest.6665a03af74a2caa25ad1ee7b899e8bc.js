90c53cc40c336338bf6dd86a5a887e2150b36889
Logan Connor <loganconnor44@gmail>
2020-06-07 17:34:53 -0400

    Updates

---
+++ precache-manifest.6665a03af74a2caa25ad1ee7b899e8bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2f2b4483bfae771ec2dd496cb45265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84127702f3434b821fc2",
+    "url": "/static/css/main.6cdd9bab.chunk.css"
+  },
+  {
+    "revision": "26027168ba446f4f11f4",
+    "url": "/static/js/2.fcc3ad80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fcc3ad80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84127702f3434b821fc2",
+    "url": "/static/js/main.7f1e9bf0.chunk.js"
+  },
+  {
+    "revision": "fe46f27525775314567a",
+    "url": "/static/js/runtime-main.90a07208.js"
+  }
+]);
